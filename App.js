291a4ed1634ad6bca6e747a0eab5def3e90f7cb5
Pamela Argentino <argentinopam@hotmail.com>
2017-08-24 18:57:28 -0300

    Add swiping motions to dismiss cards on screen

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+import Deck from './src/components/Deck';
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
 
 export default class App extends React.Component {
+  renderCard({ id, text, uri }) {
+    return  (
+      <Card
+        title={text}
+        image={{ uri }}
+        key={id}
+      >
+        <Text style={{ marginBottom: 10 }}>
+          I can customize this card further.
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03A9F4"
+          title="View Now!"
+        />
+      </Card>
+    )
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card
+        title="All done!"
+      >
+        <Text>
+          There's no more content here!
+        </Text>
+        <Button
+          backgroundColor="#03A9F4"
+          title="Get more!"
+        />
+      </Card>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+          onSwipeLeft={item => {}}
+          onSwipeRight={item => {}}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
       </View>
     );
   }
@@ -14,8 +68,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#fff'
+  }
 });
